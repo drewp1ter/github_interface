abe650e93ef1546dfdc1fdc3291e439b7eafeece
mufdvr <belora2010@yandex.ru>
2019-03-14 16:32:26 +0300

    fix

--- src/features/issuesSearching/containers/IssuesSearchingLayout/IssuesSearchingLayout.tsx
+++ src/features/issuesSearching/containers/IssuesSearchingLayout/IssuesSearchingLayout.tsx
@@ -19,7 +19,7 @@ class IssuesSearchingLayout extends React.Component<IssuesSearchingState> {
     const { issues } = this.props
     return (
       <div className={styles.layout}>
-        <Header title="GitHub Inteface" />
+        <Header title="GitHub Interface" />
         <Searching className={styles.searching} />
         <SearchingResults issues={issues} />
       </div>
