8089c191e0b31538c9e599b5d63cc131a4e6d3e1
mufdvr <belora2010@yandex.ru>
2019-03-18 10:54:14 +0300

    travis experements

--- docker_push.sh
+++ docker_push.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 #docker push mufdvr/github_interface
-docker save mufdvr/github_interface | bzip2 | ssh -o "StrictHostKeyChecking no" mika@staging.cityparkvip.ru 'bunzip2 | docker load'
+docker save mufdvr/github_interface | bzip2 | ssh -o "StrictHostKeyChecking no" mika@staging.cityparkvip.ru 'bunzip2 | docker load && ./github_interface.sh'
